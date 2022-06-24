e8b794c3cece762e5e7d2d094de5bd66b23a27f0
sekonidescendant <105178098+sekonidescendant@users.noreply.github.com>
2022-06-24 16:54:44 -0700

    js.alert

---
+++ script.js
@@ -0,0 +1,15 @@
+
+"use strict";
+
+const userName = {
+    user: "My name is Sekoni",
+    height: " I am 5'7ft tall",
+    country:" I am from Nigeria"
+};
+alert(userName.user);
+alert (userName.height);
+alert(userName.country);
+
+document.write(userName.user);
+document.write(userName.height);
+document.write(userName.country);
